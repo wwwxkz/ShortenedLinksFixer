3dcd7c6001bb927ef2a49b291b79e3a4b8bdb193
wwwxkz <wxkzwin@gmail.com>
2022-08-03 15:38:52 -0300

    Add verifications and duplicate filter

--- redirect.py
+++ redirect.py
@@ -5,18 +5,26 @@ import requests
 f = open('export.json')
 data = json.load(f)
 
+def loop_through_urls_found(urls, url_provider_base):
+    url_logger = []
+    for url in urls:
+        url = url_provider_base + url
+        # Do not show duplicates 
+        for url_log in url_logger:
+            if(url_log == url):
+                url = ''
+        # Checks for empty url
+        if(url != ''):
+            # Go to link and save redirection
+            redirect = requests.get(url)
+            print('From: ' + url + ' - To ' + redirect.url + '\n')
+            url_logger.append(url)
+
 for i in data:
+    # Regex based in my use
     url_bit_ly = re.findall("https://bit.ly/(.*?)\W", i['post_content'])
     url_tiny = re.findall("https://tinyurl.com/(.*?)\W", i['post_content'])
-    for url in url_bit_ly:
-        if(url != ''):
-            url = 'https://bit.ly/' + url
-            redirect = requests.get(url)
-            print('From: ' + url + ' - To: ' + redirect.url + '\n')
-    for url in url_tiny:
-        if(url != '' and url != 'associe'):
-            url = 'https://tinyurl.com/' + url
-            redirect = requests.get(url)
-            print('From: ' + url + ' - To: ' + redirect.url + '\n')
+    loop_through_urls_found(url_bit_ly, 'https://bit.ly/')
+    loop_through_urls_found(url_tiny, 'https://tinyurl.com/')
 
 f.close()
