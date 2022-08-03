8cf59cf14a0b26a335e4b151b28754fcbe67f7fd
wwwxkz <wxkzwin@gmail.com>
2022-08-03 16:39:39 -0300

    Added option to download media from CMS or other platforms

--- redirect.py
+++ redirect.py
@@ -6,19 +6,29 @@ f = open('export.json')
 data = json.load(f)
 
 def loop_through_urls_found(urls, url_provider_base):
+    # Header to access websites that requires it
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:71.0) Gecko/20100101 Firefox/71.0'}
+    # Log past url cases, if it appears again, do nothing
     url_logger = []
     for url in urls:
-        url = url_provider_base + url
+        url_short = url_provider_base + url
         # Do not show duplicates 
         for url_log in url_logger:
             if(url_log == url):
-                url = ''
+                url_short = ''
         # Checks for empty url
-        if(url != ''):
-            # Go to link and save redirection
-            redirect = requests.get(url)
-            print('From: ' + url + ' - To ' + redirect.url + '\n')
-            url_logger.append(url)
+        if(url_short != ''):
+            redirect = requests.get(url_short, allow_redirects=True, headers=headers)
+            # If it is in CMS images/ folder download it
+            if ('images' in redirect.url):
+                with open(redirect.url.split('images/')[1], 'wb') as blob:
+                    # For bigger filles it would be better increasing chunk size
+                    for chunk in redirect.iter_content(1024):
+                        blob.write(chunk)
+            else:
+                # Print redirections end
+                print('From: ' + url + ' - To ' + redirect.url + '\n')
+                url_logger.append(url)
 
 for i in data:
     # Regex based in my use
