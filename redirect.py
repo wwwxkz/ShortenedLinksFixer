6e1864004926c510e4ea86b1b5b0f9290f57aa61
wwwxkz <wxkzwin@gmail.com>
2022-08-04 09:57:05 -0300

    Improved regex and duplicates logic

--- redirect.py
+++ redirect.py
@@ -2,38 +2,40 @@ import json
 import re
 import requests
 
-def loop_through_urls_found(urls, url_provider_base):
-    # Log past url cases, if it appears again, do nothing
-    url_logger = []
-    # Header to access websites that requires it
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:71.0) Gecko/20100101 Firefox/71.0'}
-    for url in urls:
-        url_short = url_provider_base + url
-        # Do not show duplicates 
-        for url_in_logger in url_logger:
-            if(url_in_logger == url):
-                continue
-        # Checks for empty url
-        if(url_short != ''):
-            redirect = requests.get(url_short, allow_redirects=True, headers=headers)
-            # If it is in CMS images/ folder download it
-            if ('images' in redirect.url):
-                with open(redirect.url.split('images/')[1], 'wb') as blob:
-                    # For bigger filles it would be better increasing chunk size
-                    for chunk in redirect.iter_content(1024):
-                        blob.write(chunk)
-            else:
-                # Print redirections end
-                print('From: ' + url + ' - To ' + redirect.url + '\n')
-            url_logger.append(url)
+def find_shortened_links_in_post(posts, shortener_provider):
+	urls = []
+	for post in posts:
+		# Find text starting with shortener_provider and stop at a not word character
+		for url in re.findall(shortener_provider + "(.*?)\W", post['post_content']):
+			urls.append(url)
+	return list(dict.fromkeys(urls))
 
-f = open('export.json')
-data = json.load(f)
-for i in data:
-    # Regex based in my use
-    url_bit_ly = re.findall("https://bit.ly/(.*?)\W", i['post_content'])
-    url_tiny = re.findall("https://tinyurl.com/(.*?)\W", i['post_content'])
-    loop_through_urls_found(url_bit_ly, 'https://bit.ly/')
-    loop_through_urls_found(url_tiny, 'https://tinyurl.com/')
+def loop_through_shortened_links(urls, shortener_provider):
+	# Header to access websites that requires it
+	headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:71.0) Gecko/20100101 Firefox/71.0'}
+	for url in urls:
+		url_short = shortener_provider + url
+		# Checks for empty url
+		if(url_short != ''):
+			redirect = requests.get(url_short, allow_redirects=True, headers=headers)
+			# If it is in CMS images/ folder download it
+			if ('images' in redirect.url):
+				with open(redirect.url.split('images/')[1], 'wb') as blob:
+					# For bigger filles it would be better increasing chunk size
+					for chunk in redirect.iter_content(1024):
+						blob.write(chunk)
+			else:
+				# Print redirections
+				print('From: ' + url + ' - To ' + redirect.url + '\n')
 
-f.close()
+file = open('export.json')
+posts = json.load(file)
+
+urls_bit_ly = find_shortened_links_in_post(posts, 'https://bit.ly/')
+urls_tiny = find_shortened_links_in_post(posts, 'https://tinyurl.com/')
+
+# Turn array into dict to delete duplicates	
+loop_through_shortened_links(urls_bit_ly, 'https://bit.ly/')
+loop_through_shortened_links(urls_tiny, 'https://tinyurl.com/')
+
+file.close()
